0a35670f1ac5e6a4cc4c2a56a4302dd8e7ca45fa
admin <chaseohm@gmail.com>
2019-01-09 15:10:42 -0600

    final

---
+++ src/MethodsExercises.java
@@ -0,0 +1,10 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+        addition(2 , 3);
+        System.out.println(addition(2, 3));
+    }
+
+    public static int addition( int a, int b){
+        return a + b;
+    }
+}
